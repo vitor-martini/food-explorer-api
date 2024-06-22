31edfdf292d836a3b6e399f1fe650ded9161a5a0
Vitor Martini <vitor-martini@outlook.com>
2024-06-22 20:31:23 -0300

    feat: search by user email

--- app/Repositories/UserRepository.js
+++ app/Repositories/UserRepository.js
@@ -4,6 +4,9 @@ const PgRepository = use('App/Repositories/PgRepository')
 const User = use('App/Models/User')
 
 class UserRepository extends PgRepository {
+  async findByEmail(email) {
+    return await this.model.findBy('email', email)
+  }
 }
 
 module.exports = new UserRepository(User)
