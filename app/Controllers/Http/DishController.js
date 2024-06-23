01a4cac4b748b425929768e9963a0660ca65357e
Vitor Martini <vitor-martini@outlook.com>
2024-06-23 12:19:12 -0300

    feat: ingredient register when add nem dish

--- app/Controllers/Http/DishController.js
+++ app/Controllers/Http/DishController.js
@@ -1,11 +1,13 @@
 const dishRepository = use('App/Repositories/DishRepository')
 const categoryRepository = use('App/Repositories/CategoryRepository')
+const ingredientRepository = use('App/Repositories/IngredientRepository')
+const dishIngredientRepository = use('App/Repositories/DishIngredientRepository')
 const DishService = use('App/Services/DishService')
-const dishService = new DishService(dishRepository, categoryRepository)
+const dishService = new DishService(dishRepository, categoryRepository, ingredientRepository, dishIngredientRepository)
 
 class DishController {
   async create({ request, response }) {
-    const dishData = request.only(['name', 'category_id', 'price', 'description'])
+    const dishData = request.only(['name', 'category_id', 'price', 'description', 'ingredients'])
     const dish = await dishService.create(dishData)
     return response.status(201).json(dish)
   }
--- app/Services/DishService.js
+++ app/Services/DishService.js
@@ -2,13 +2,15 @@ const AppService = use('App/Services/AppService')
 const AppException = use('App/Exceptions/AppException')
 
 class DishService extends AppService {
-  constructor(dishRepository, categoryRepository) {
+  constructor(dishRepository, categoryRepository, ingredientRepository, dishIngredientRepository) {
     super(dishRepository)
     this.categoryRepository = categoryRepository
+    this.ingredientRepository = ingredientRepository
+    this.dishIngredientRepository = dishIngredientRepository 
   }
 
-  async create({name, category_id, price, description}) {
-    if(!name || !category_id || !price || !description) {
+  async create({name, category_id, price, description, ingredients}) {
+    if(!name || !category_id || !price || !description || !ingredients || ingredients.length === 0) {
       throw new AppException('Required fields not informed', 400)
     }
 
@@ -21,8 +23,27 @@ class DishService extends AppService {
     if(!isValidCategory) {
       throw new AppException('Invalid category', 400)
     }
+    
+    const dish = await this.repository.create({name, category_id, price, description})
 
-    return await this.repository.create({name, category_id, price, description})
+    ingredients = [...new Set(ingredients.map(ingredient =>  ingredient = ingredient.toLowerCase()))]
+    const checkIngredientsPromises = ingredients.map(async (ingredient) => {
+      const find = await this.ingredientRepository.findByName(ingredient)
+      let ingredient_id
+      if(find) {
+        ingredient_id = find.id
+      } else {
+        const newIngredient = await this.ingredientRepository.create({ name: ingredient })
+        ingredient_id = newIngredient.id
+      }
+
+      await this.dishIngredientRepository.create({ dish_id: dish.id, ingredient_id })
+    })
+
+    await Promise.all(checkIngredientsPromises)
+    dish.ingredients = ingredients
+
+    return dish
   }
 }
 
