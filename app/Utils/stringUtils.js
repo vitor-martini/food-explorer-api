b8b603745a5d69919c086da9b74ec9b06158ae5c
Vitor Martini <vitor-martini@outlook.com>
2024-06-23 12:16:05 -0300

    feat: string utils

---
+++ app/Utils/stringUtils.js
@@ -0,0 +1,8 @@
+function capitalizeFirstLetter(string) {
+  if(!string) return ''
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+module.exports = {
+  capitalizeFirstLetter
+}
